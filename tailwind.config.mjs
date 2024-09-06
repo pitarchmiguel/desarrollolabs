082c94513b43f81c78a1f139d702b49c051a4270
pitarchmiguel <pitarchmiguel@gmail.com>
2024-09-06 22:41:55 +0200

    init commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			'ast-global-color-0': '#fbe8a6',
+			'ast-global-color-1': '#f3d97a',
+			'ast-global-color-2': '#2c2927',
+			'ast-global-color-3': '#534a45',
+			'ast-global-color-4': '#f2f1e5',
+			'ast-global-color-5': '#ffffff',
+			'ast-global-color-6': '#f2f5f7',
+			'ast-global-color-7': '#2c2927',
+			'ast-global-color-8': '#000000',
+	},
+	plugins: [],
+}
+}
